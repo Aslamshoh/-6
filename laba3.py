5399b54338b7480d2c1e02a3d1696d82387bbcf6
Aslamshoh <159581247+Aslamshoh@users.noreply.github.com>
2024-05-11 05:02:21 +0400

    Update laba3.py

--- laba3.py
+++ laba3.py
@@ -3,46 +3,19 @@
 несимметрично. При этом матрица А не меняется. После чего вычисляется выражение
 ((К*A T)*(F+А)-K* F T . Выводятся по мере формирования А, F и все матричные операции последовательно
 import numpy as np
+import zlib
 
-
-# Функция для проверки, является ли число простым
-def is_prime(n):
-    if n <= 1:
-        return False
-    for i in range(2, int(np.sqrt(n)) + 1):
-        if n % i == 0:
-            return False
-    return True
-
-
-# Функция для формирования матрицы F
-def form_matrix_F(matrix_B, matrix_C):
-    num_primes_in_B = np.sum([is_prime(x) for x in matrix_B.flatten()])
-
-    if num_primes_in_B == 3:
-        matrix_B_symmetric = np.flip(matrix_B, axis=1)
-        matrix_B[:, 0], matrix_B[:, 2] = matrix_B_symmetric[:, 2], matrix_B_symmetric[:, 0]
-    else:
-        matrix_B, matrix_C = matrix_C.copy(), matrix_B.copy()
-
-    return matrix_B, matrix_C
-
-
-# Функция для вычисления выражения ((К*A.T)*(F+A)-K*F.T)
-def compute_expression(K, matrix_A, matrix_F):
-    result = np.dot(K * matrix_A.T, matrix_F + matrix_A) - K * matrix_F.T
-    return result
-
-
-# Генерация матрицы А
-def generate_matrix_A(N):
-    return np.random.randint(-10, 11, (N, N))
-
-
-# Основная функция
+# Функция для поиска числителей в матрице
+def find_divisible_numbers(matrix, divisor):
+    divisible_numbers = []
+    for number in matrix.flatten():
+        if number % divisor == 0:
+            divisible_numbers.append(number)
+    return divisible_numbers
 def main():
     K = float(input("Введите значение K: "))
     N = int(input("Введите размерность матрицы А (N): "))
+    divisor = int(input("Введите число, на которое будем проверять деление: "))
 
     matrix_A = generate_matrix_A(N)
     matrix_B = generate_matrix_A(N)
@@ -66,7 +39,18 @@ def main():
     print("\nРезультат вычисления выражения ((К*A.T)*(F+A)-K*F.T):")
     print(expression_result)
 
+    divisible_numbers_A = find_divisible_numbers(matrix_A, divisor)
+    print(f"\nЧислители в матрице A, делящиеся на {divisor} без остатка:")
+    print(divisible_numbers_A)
+
+    divisible_numbers_B = find_divisible_numbers(matrix_B, divisor)
+    print(f"\nЧислители в матрице B, делящиеся на {divisor} без остатка:")
+    print(divisible_numbers_B)
+
+    divisible_numbers_C = find_divisible_numbers(matrix_C, divisor)
+    print(f"\nЧислители в матрице C, делящиеся на {divisor} без остатка:")
+    print(divisible_numbers_C)
+
 
 if __name__ == "__main__":
     main()
-
